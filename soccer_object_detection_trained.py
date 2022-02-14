a76fb1fe534fbceec4646839dc4d691ccee9b986
JacobZoarets <yacovz@gmail.com>
2022-02-14 23:06:20 +0200

    Add files via upload

---
+++ soccer_object_detection_trained.py
@@ -0,0 +1,335 @@
+
+import os
+os.chdir('/content/object_detection/')
+
+# Check current working directory.
+retval = os.getcwd()
+print ("Current working directory %s" % retval)
+!ls
+
+"""# Step 1
+**Object Detection API configuration**: In this step, the model for object detection is downloaded, also some copying and deletion of references are done to leave the whole scheme configured.
+"""
+
+!git clone https://github.com/tensorflow/models.git
+!apt-get -qq install libprotobuf-java protobuf-compiler
+!protoc ./models/research/object_detection/protos/string_int_label_map.proto --python_out=.
+!cp -R models/research/object_detection/ object_detection/
+!rm -rf models
+
+"""# Step 2
+
+**Imports** needed to run the Object Detection API demonstration
+"""
+
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+
+from PIL import Image
+
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+
+"""# Step 3
+**Configuration** of the model to use, path to the frozen inference graph and extra config elements for the Object detection API implementation.
+"""
+
+# What model to download.
+# MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
+
+# model with more accurancy but up to you use a diferent model
+MODEL_NAME = 'faster_rcnn_inception_resnet_v2_atrous_coco_2017_11_08'
+
+MODEL_FILE = MODEL_NAME + '.tar.gz'
+DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = os.path.join('object_detection/data', 'mscoco_label_map.pbtxt')
+
+NUM_CLASSES = 90
+
+opener = urllib.request.URLopener()
+opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+tar_file = tarfile.open(MODEL_FILE)
+for file in tar_file.getmembers():
+  file_name = os.path.basename(file.name)
+  if 'frozen_inference_graph.pb' in file_name:
+    tar_file.extract(file, os.getcwd())
+    
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+    
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+"""# Step 4
+Section with demo images enviroment
+"""
+
+# For the sake of simplicity we will use only 2 images:
+# image1.jpg
+# image2.jpg
+# If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
+PATH_TO_TEST_IMAGES_DIR = '/content/object_detection/images'
+TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image_{}.jpg'.format(i)) for i in range(1, 2) ]
+
+# Size, in inches, of the output images.
+IMAGE_SIZE = (12, 8)
+
+"""# Step 5
+piece of code that represent the concrete detection, calling the TF session
+"""
+
+with detection_graph.as_default():
+  with tf.Session(graph=detection_graph) as sess:
+    # Definite input and output Tensors for detection_graph
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+    # Each box represents a part of the image where a particular object was detected.
+    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+    # Each score represent how level of confidence for each of the objects.
+    # Score is shown on the result image, together with the class label.
+    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+    for image_path in TEST_IMAGE_PATHS:
+      image = Image.open(image_path)
+      # the array based representation of the image will be used later in order to prepare the
+      # result image with boxes and labels on it.
+      image_np = load_image_into_numpy_array(image)
+      # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+      image_np_expanded = np.expand_dims(image_np, axis=0)
+      # Actual detection.
+      (boxes, scores, classes, num) = sess.run(
+          [detection_boxes, detection_scores, detection_classes, num_detections],
+          feed_dict={image_tensor: image_np_expanded})
+      # Visualization of the results of a detection.
+      vis_util.visualize_boxes_and_labels_on_image_array(
+          image_np,
+          np.squeeze(boxes),
+          np.squeeze(classes).astype(np.int32),
+          np.squeeze(scores),
+          category_index,
+          use_normalized_coordinates=True,
+          line_thickness=8)
+      plt.figure(figsize=IMAGE_SIZE)
+      plt.imshow(image_np)
+
+"""# Custom Personal Images """
+
+!mkdir images
+
+!wget http://lbi.co.il/projects/accessibility/images/image_2.jpg -O images/image_2.jpg
+
+!wget http://farm4.staticflickr.com/3674/9412766091_03a2e22443_z.jpg -O images/custom_img_1.jpg
+
+!ls #./images/
+
+PATH_TO_TEST_IMAGES_DIR = 'images'
+TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image_{}.jpg'.format(i)) for i in range(1, 3) ]
+
+IMAGE_SIZE = (12, 8)
+
+with detection_graph.as_default():
+  with tf.Session(graph=detection_graph) as sess:
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+    for image_path in TEST_IMAGE_PATHS:
+      image = Image.open(image_path)
+      image_np = load_image_into_numpy_array(image)
+      image_np_expanded = np.expand_dims(image_np, axis=0)
+      (boxes, scores, classes, num) = sess.run(
+          [detection_boxes, detection_scores, detection_classes, num_detections],
+          feed_dict={image_tensor: image_np_expanded})
+      vis_util.visualize_boxes_and_labels_on_image_array(
+          image_np,
+          np.squeeze(boxes),
+          np.squeeze(classes).astype(np.int32),
+          np.squeeze(scores),
+          category_index,
+          use_normalized_coordinates=True,
+          line_thickness=8)
+      plt.figure(figsize=IMAGE_SIZE)
+      plt.imshow(image_np)
+
+"""## test detection"""
+
+!ls
+
+!wget http://lbi.co.il/clients/mody/soccer_object_detection.tar
+!tar -xvf soccer_object_detection.tar
+
+!wget http://lbi.co.il/clients/mody/utils.tar
+!tar -xvf utils.tar
+
+!wget http://lbi.co.il/clients/mody/object_detection.tar
+!tar -xvf object_detection.tar
+
+from google.colab import files
+
+uploaded = files.upload()
+
+for fn in uploaded.keys():
+  print('User uploaded file "{name}" with length {length} bytes'.format(
+      name=fn, length=len(uploaded[fn])))
+
+######## Image Object Detection Using Tensorflow-trained Classifier #########
+#
+# Author: Evan Juras
+# Date: 1/15/18
+# Description: 
+# This program uses a TensorFlow-trained classifier to perform object detection.
+# It loads the classifier uses it to perform object detection on an image.
+# It draws boxes and scores around the objects of interest in the image.
+
+## Some of the code is copied from Google's example at
+## https://github.com/tensorflow/models/blob/master/research/object_detection/object_detection_tutorial.ipynb
+
+## and some is copied from Dat Tran's example at
+## https://github.com/datitran/object_detector_app/blob/master/object_detection_app.py
+
+## but I changed it to make it more understandable to me.
+
+# Import packages
+import os
+import cv2
+import numpy as np
+import tensorflow as tf
+import sys
+
+# This is needed since the notebook is stored in the object_detection folder.
+sys.path.append("..")
+
+# Import utilites
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+
+# Name of the directory containing the object detection module we're using
+MODEL_NAME = 'soccer_object_detection'
+IMAGE_NAME = 'image_63_1.jpg'
+
+# Grab path to current working directory
+CWD_PATH = os.getcwd()
+
+print("CWD_PATH")
+print(CWD_PATH)
+
+# Path to frozen detection graph .pb file, which contains the model that is used
+# for object detection.
+PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'exported_graphs/frozen_inference_graph.pb')
+
+print("PATH_TO_CKPT")
+print(CWD_PATH)
+
+# Path to label map file
+PATH_TO_LABELS = os.path.join(CWD_PATH,MODEL_NAME,'label_map.pbtxt')
+
+print("PATH_TO_LABELS")
+print(PATH_TO_LABELS)
+
+# Path to image
+PATH_TO_IMAGE = os.path.join(CWD_PATH,IMAGE_NAME)
+
+print("PATH_TO_LABELS")
+print(PATH_TO_LABELS)
+
+# Number of classes the object detector can identify
+NUM_CLASSES = 1
+
+# Load the label map.
+# Label maps map indices to category names, so that when our convolution
+# network predicts `5`, we know that this corresponds to `king`.
+# Here we use internal utility functions, but anything that returns a
+# dictionary mapping integers to appropriate string labels would be fine
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+# Load the Tensorflow model into memory.
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+    sess = tf.Session(graph=detection_graph)
+
+# Define input and output tensors (i.e. data) for the object detection classifier
+
+# Input tensor is the image
+image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+# Output tensors are the detection boxes, scores, and classes
+# Each box represents a part of the image where a particular object was detected
+detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+# Each score represents level of confidence for each of the objects.
+# The score is shown on the result image, together with the class label.
+detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+
+# Number of objects detected
+num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+# Load image using OpenCV and
+# expand image dimensions to have shape: [1, None, None, 3]
+# i.e. a single-column array, where each item in the column has the pixel RGB value
+image = cv2.imread(PATH_TO_IMAGE)
+image_expanded = np.expand_dims(image, axis=0)
+
+# Perform the actual detection by running the model with the image as input
+(boxes, scores, classes, num) = sess.run(
+    [detection_boxes, detection_scores, detection_classes, num_detections],
+    feed_dict={image_tensor: image_expanded})
+
+# Draw the results of the detection (aka 'visulaize the results')
+
+print("boxes")
+print(boxes)
+print("scores")
+print(scores)
+print("classes")
+print(classes)
+print("num")
+print(num)
+
+vis_util.visualize_boxes_and_labels_on_image_array(
+    image,
+    np.squeeze(boxes),
+    np.squeeze(classes).astype(np.int32),
+    np.squeeze(scores),
+    category_index,
+    use_normalized_coordinates=True,
+    line_thickness=2,
+    min_score_thresh=0.00)
+
+# All the results have been drawn on image. Now display the image.
+cv2.imshow('Object detector', image)
+
+# Press any key to close the image
+#cv2.waitKey(0)
+
+# Clean up
+#cv2.destroyAllWindows()
